78334cc81b37ea7fc7ecb4ab1e97aefec58fa490
NathanSchot <56635731+NathanSchot@users.noreply.github.com>
2021-04-29 14:39:00 +0200

    Added Comments to files

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 # https://pandas.pydata.org/docs/getting_started/intro_tutorials/index.html
 cardata = pd.read_excel('./data/data.xlsx')
 cardata = cardata.iloc[0:10, :]  # Get first 10 rows only for now
+# Maybe highlight what the use for to_dict is here?
 cardata = cardata.to_dict(orient='records')
 _id = len(cardata)
 
@@ -23,10 +24,12 @@ def _():
 # Data page
 @app.route('/data')
 def _data():
+    # Not sure what the extra inputs are doing, but also not sure whether we should go into so much detail
     return render_template('data.html', data=cardata, _id=_id)
 
 
-# Detail page for one data object (a car)
+# Detail page for one data object (a car), after clicking the id on the data page
+# Maybe mention that the click functionality is defined in the html file, not here?
 @ app.route('/data/<id>')
 def _data_id(id):
     car = [c for c in cardata if c['ID'] == int(id)][0]
@@ -39,11 +42,12 @@ def _data_id(id):
 def _add_new_data():
     global _id
     _id += 1
-    form = request.form.to_dict()
+    form = request.form.to_dict()  # The data from the page enters here
     form['ID'] = _id
     cardata.append(form)
-    return redirect('/data')
+    return redirect('/data')  # Refreshes the page?
 
 
 # More Flask stuff, this runs the server on http://localhost:5000
+# Maybe highlight that the debug=True allows you to makes and save changes that you will see right away
 app.run(host='localhost', port=5000, debug=True)
